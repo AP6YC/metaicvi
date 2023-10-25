6d64cb7c8039c54b4a13839c8ae3b389e07ae588
Sasha Petrenko <sap625@umsystem.edu>
2023-10-25 16:29:16 -0500

    Add data used in compat demo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../src/metaicvi'))
 # -- Project information -----------------------------------------------------
 
 project = 'metaicvi'
-copyright = '2022, Sasha Petrenko'
+copyright = '2023, Sasha Petrenko'
 author = 'Sasha Petrenko'
 
 # The full version, including alpha/beta/rc tags
